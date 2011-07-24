c46b0c3c3434cffab2536afc518b6e8bd5d58da1
Logan Buesching <logan@agoln.net>
2011-07-24 10:34:29 -0700

    Empty rails app to begin with

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashvolunteer::Application.config.secret_token = 'ced2ec08e129b74ad14266e62e40fb51db4a25e4ff34469c182f1104b9b62e9de0188c9e1fa15a88709a16191cdc1e22b3e6ef9ff1ca81944b307d9095a679ec'
