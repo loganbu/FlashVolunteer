7f7623edbe10bb6d640af87eef1dcfad1a0a067d
Logan Buesching <logan@agoln.net>
2011-12-10 16:30:38 -0800

    Add creator to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ActiveRecord::Base
     belongs_to :neighborhood, :foreign_key => "Neighborhood_id"
+    belongs_to :user, :foreign_key => "creator_id"
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,7 @@ Flashvolunteer::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+  
+  config.log_level = :fatal
 end
 
