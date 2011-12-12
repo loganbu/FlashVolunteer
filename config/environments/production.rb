8a7d164b11c80baa2e88d3c62b8e6d275f72f867
Logan Buesching <logan@agoln.net>
2011-12-11 19:50:08 -0800

    Add mailer settings to prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,19 @@
 Flashvolunteer::Application.configure do
+    require 'tlsmail' 
+		Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE) 
+		ActionMailer::Base.delivery_method = :smtp 
+		ActionMailer::Base.perform_deliveries = true 
+		ActionMailer::Base.raise_delivery_errors = true 
+		ActionMailer::Base.smtp_settings = { 
+				:address => "smtp.gmail.com", 
+				:port => "587", 
+				:domain => "gmail.com", 
+				:enable_starttls_auto => true, 
+				:authentication       => "plain",
+				#:user_name => "gmail_account_from@gmail.com", 
+				#:password => "gmail_account_password" 
+		} 
+		
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
