d4c047de2a6810b5d2bef73a80e3833e294a9c2f
Logan Buesching <logan@agoln.net>
2011-12-14 18:33:48 -0800

    Fix 'confirm account' bug and signing in not having correct form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Flashvolunteer::Application.routes.draw do
-  devise_for :users, :controllers => { :confirmations => "users/confirmations", :registrations => "users/registrations" } do
+  devise_for :users, :controllers => { :confirmations => "users/confirmations", :registrations => "users/registrations", :sessions => "users/sessions" } do
 		root :to => "events#index"
     put "confirm_account", :to => "users/confirmations#confirm_account"
 		get "users/sign_up/quick", :to => "users/registrations#quick", :as => "quick_new_user"
