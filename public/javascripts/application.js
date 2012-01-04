70c1966222e9560cbd6b0bf11446597be10a6a1b
Logan Buesching <logan@agoln.net>
2012-01-03 20:37:57 -0800

    Fix points on the map for pagination

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,3 +9,12 @@ function gotoNeighborhood(selection) {
 function submitForm(id) {
    var form = document.forms[id].submit();
 }
+
+function getParameterByName(name) { 
+ 
+    var match = RegExp('[?&]' + name + '=([^&]*)') 
+                    .exec(window.location.search); 
+ 
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' ')); 
+ 
+} 
