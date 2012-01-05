4771cb67f68bd312f0d521c4353233fcdf28dd5f
agoln <agolna@gmail.com>
2012-01-04 22:35:23 -0800

    Fixed #21,#22 by including a facebook and twitter URL

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,8 @@
 module EventsHelper
+  def twitter_url(event)
+    "http://twitter.com/home?source=" + root_url + "&status=Thinking%20about%20volunteering%20at%20" + event_url(event) + "%20via%20@flashvolunteer.%20Anyone%20want%20to%20join?"
+  end
+  def facebook_url(event)
+    "http://www.facebook.com/sharer.php?u=" + event_url(event)
+  end
 end
