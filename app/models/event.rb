e410cf413dbf2a96d7d68ca3c61f5305554169bf
agoln <agolna@gmail.com>
2012-01-19 21:36:26 -0800

    Fixed #71 - validate all fields for event creation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,15 +1,22 @@
 class Event < ActiveRecord::Base
-  belongs_to :neighborhood, :foreign_key => "neighborhood_id"
-  belongs_to :user, :foreign_key => "creator_id"
-  validates :neighborhood_id, :presence => true
-	has_and_belongs_to_many :participants, :class_name => "User", :join_table => "events_users", :uniq => true
-	
-	geocoded_by :geocode_address
+    belongs_to :neighborhood, :foreign_key => "neighborhood_id"
+    belongs_to :user, :foreign_key => "creator_id"
+    validates :neighborhood_id, :presence => true
+    validates :name, :presence => { :message => "The event must have a title" }
+    validates :street, :presence => { :message => "The event must have an address" }
+    validates :start, :presence => { :message => "The event must have a start time" }
+    validates :start, :date => { :after => Time.now, :message=> "The event must begin in the future" }, :on => :create
+    validates :end, :presence => { :message => "The event must have an end time" }, :date  => { :after => :start, :message=> "The event must end after the start time" }
+    validates :description, :presence => { :message => "The event must have a description" }
+    has_and_belongs_to_many :participants, :class_name => "User", :join_table => "events_users", :uniq => true
 
-	after_validation :geocode
-	
-	def geocode_address
-		return "#{self.street} #{self.city}, #{self.zip} #{self.state}"
-	end
+
+    geocoded_by :geocode_address
+
+    after_validation :geocode
+
+    def geocode_address
+	   return "#{self.street} #{self.city}, #{self.zip} #{self.state}"
+    end
 	
 end
