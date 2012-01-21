3e90cbbb44bf0a41fa182ed7c7d935cde7bc7809
agoln <agolna@gmail.com>
2012-01-21 12:33:48 -0800

    Fixed #30 by making neighborhoods background the image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     render :file => "app/views/shared/authfail.html.erb"
   end
+
+private
+    before_filter :create_action_and_controller
+
+  def create_action_and_controller
+    @current_action = action_name
+    @current_controller = controller_name
+  end
 end
