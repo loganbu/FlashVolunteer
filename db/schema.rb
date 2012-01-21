eaaeaab1eaec0c24ad328a9a9832d083e6718e73
agoln <agolna@gmail.com>
2012-01-21 15:09:00 -0800

    Fixed #31 - add a 'special instructions' field to the event

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120115004638) do
+ActiveRecord::Schema.define(:version => 20120121230552) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -28,8 +29,9 @@ ActiveRecord::Schema.define(:version => 20120115004638) do
     t.integer  "zip"
     t.float    "latitude"
     t.float    "longitude"
-    t.string   "state",           :default => "WA"
+    t.string   "state",                :default => "WA"
     t.string   "website"
+    t.text     "special_instructions"
   end
 
   create_table "events_users", :id => false, :force => true do |t|
