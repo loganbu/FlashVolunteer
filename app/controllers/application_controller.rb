7e2d07f225bfc8879f84b15c818931b48569c88c
agoln <agolna@gmail.com>
2012-01-21 13:51:22 -0800

    Fixed #70 by redirecting users to correct pages after signin in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
   protect_from_forgery
   check_authorization
 
   rescue_from CanCan::AccessDenied do |exception|
     render :file => "app/views/shared/authfail.html.erb"
+  end
+
+
+  def after_sign_in_path_for(resource_or_scope)
+    store_location = get_store_location
+    clear_store_location
+    (store_location.nil?) ? root_url : store_location.to_s
   end
 
 private
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,38 @@
+module SessionsHelper
+
+  def store_url(url)
+    session[:return_to] = url
+  end
+  def send_to_quick_signup(url)
+    store_location(url)
+    redirect_to quick_new_user_url
+  end
+
+  def send_to_signin(url)
+    store_location(url)
+    redirect_to new_user_session_url
+  end
+
+  def send_to_full_signup(url)
+    store_location(url)
+    redirect_to new_user_registration_url
+  end
+
+  def anyone_signed_in?
+    !current_user.nil?
+  end
+
+  def get_store_location
+    session[:return_to]
+  end
+
+  def clear_store_location
+    session[:return_to] = nil
+  end
+
+  private
+    def store_location(url)
+      session[:return_to] = url ? url : request.fullpath
+    end
+
+end
