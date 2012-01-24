f434a168fc3d0f094da1dd5077bf5c87aed97a4c
agoln <agolna@gmail.com>
2012-01-23 22:41:58 -0800

    Show map on the event description page

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -15,47 +15,57 @@ Map.setMap = function (div, mapLatitude, mapLongitude, zoom) {
     gMap = new google.maps.Map(document.getElementById(div), myOptions);
 };
 
-Map.addPoints = function (urlSource) {
+
+Map.showMapFromElement = function(element, i, eventCallback) {
+    var latitude = 0;
+    var longitude = 0;
+    latitude = element.find('latitude').text();
+    longitude = element.find('longitude').text();
+    name = element.find('name').text();
+    var attending=element.find('user-participates').text();
+    var imageStrip = attending == "true" ? "/assets/green_markers.png" : "/assets/red_markers.png"; 
+    var latLng = new google.maps.LatLng(latitude, longitude);
+
+    // Create a custom marker icon
+    var icon = new google.maps.MarkerImage(
+        imageStrip,
+        new google.maps.Size(20, 20), // Image size
+        new google.maps.Point(0, i*20), // Sprite origin
+        new google.maps.Point(10, 10) // Mark the item in the middle
+        );
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: gMap,
+        title: name,
+        animation: google.maps.Animation.DROP,
+        icon: icon
+        });
+
+    var eventXml = element;
+    var eventId = eventXml.find('id').text();
+    eventMarkers[eventId] = marker;
+
+    if (eventCallback) {
+        google.maps.event.addListener(marker, 'click', function () {
+            eventCallback(eventId);
+        });
+    }
+}
+
+Map.addPoints = function (urlSource, eventCallback) {
     $.ajax({
         url: urlSource,
         dataType: 'xml'
     }).success(function (data) {
-        var latitude = 0;
-        var longitude = 0;
         $(data).find('event').each(function(i) {
-            latitude = $(this).find('latitude').text();
-            longitude = $(this).find('longitude').text();
-            name = $(this).find('name').text();
-            var attending=$(this).find('user-participates').text();
-            var imageStrip = attending == "true" ? "/assets/green_markers.png" : "/assets/red_markers.png"; 
-            var latLng = new google.maps.LatLng(latitude, longitude);
-
-            // Create a custom marker icon
-            var icon = new google.maps.MarkerImage(
-                imageStrip,
-                new google.maps.Size(20, 20), // Image size
-                new google.maps.Point(0, i*20), // Sprite origin
-                new google.maps.Point(10, 10) // Mark the item in the middle
-                );
-            var marker = new google.maps.Marker({
-                position: latLng,
-                map: gMap,
-                title: name,
-                animation: google.maps.Animation.DROP,
-                icon: icon
-                });
-
-            var eventXml = $(this);
-            var eventId = eventXml.find('id').text();
-            eventMarkers[eventId] = marker;
-
-            google.maps.event.addListener(marker, 'click', function () {
-                Map.showPopup(eventId);
-            });
+            Map.showMapFromElement($(this), i, eventCallback)
         });
     });
 };
 
+Map.centerOnEvent = function(eventId) {
+    // Goto events page, centered on eventId
+}
 Map.showPopup = function(eventId) {
     if (previousInfoWindow) {
         previousInfoWindow.close();
