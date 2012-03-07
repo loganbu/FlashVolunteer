30b8b2ad3b2e61022e36f0648c3af2c56611f84f
agoln <agolna@gmail.com>
2012-03-06 22:02:55 -0800

    Google auth?

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'aws-sdk'
 gem 'aws-s3'
 gem "paperclip"
 gem "omniauth-facebook"
+gem "omniauth-google-oauth2"
 
 # http://www.imagemagick.com/www/binary-releases.html#windows
 # http://stackoverflow.com/questions/4451213/ruby-1-9-2-how-to-install-rmagick-on-windows
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,6 +10,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
           session["devise.facebook_data"] = request.env["omniauth.auth"]
           redirect_to new_user_registration_url
         end
+    end
 
+    def google_oauth2
+        # You need to implement the method below in your model
+        @user = User.find_for_google_oauth(request.env["omniauth.auth"], current_user)
+
+        if @user.persisted?
+          sign_in_and_redirect @user
+        else
+          session["devise.google_data"] = request.env["omniauth.auth"]
+          redirect_to new_user_registration_url
+        end
     end
 end
