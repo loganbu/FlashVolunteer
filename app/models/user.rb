4108e537eed00e6224480db41f30b3224d1c13f1
agoln <agolna@gmail.com>
2012-03-06 22:11:38 -0800

    Add method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,5 +42,13 @@ class User < ActiveRecord::Base
       User.create!(:email => data.email, :password => Devise.friendly_token[0,20]) 
     end
   end
+  def self.find_for_google_oauth(access_token, signed_in_resource=nil)
+    data = access_token.extra.raw_info
+    if user = User.where(:email => data.email).first
+      user
+    else # Create a user with a stub password. 
+      User.create!(:email => data.email, :password => Devise.friendly_token[0,20]) 
+    end
+  end
 
 end
