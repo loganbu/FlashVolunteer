24dc4ab447d12b3f541529e930836be49c9ba3ac
agoln <agolna@gmail.com>
2012-04-16 21:10:36 -0700

    Add hashtags to the event data, update share links

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120401180623) do
+ActiveRecord::Schema.define(:version => 20120417035646) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20120401180623) do
     t.string   "state",                :default => "WA"
     t.string   "website"
     t.text     "special_instructions"
+    t.string   "twitter_hashtags"
   end
 
   create_table "neighborhoods", :force => true do |t|
