1908b3ed681b6480e28ef3b90c4d4d11d9f2ea70
agoln <agolna@gmail.com>
2012-05-05 19:14:54 -0700

    Staging doesn't output to a file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -21,8 +21,6 @@ Flashvolunteer::Application.configure do
     config.action_mailer.default_url_options = { :host => ENV['SITE_DOMAIN'] }
 
     config.action_mailer.raise_delivery_errors = true 
-    config.action_mailer.delivery_method = :file 
-
         
   # Settings specified here will take precedence over those in config/application.rb
 
