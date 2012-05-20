aa397adf1321bbdba567f93ea5b5cc229951745f
agoln <agolna@gmail.com>
2012-05-20 14:32:35 -0700

    When not authenticated, add user to event as soon as authenticated

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,6 +7,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     def create
         super
+        if (session[:sign_up_for_event])
+            event = Event.find(session[:sign_up_for_event])
+            if (event != nil)
+                event.participants << current_user if !event.participants.include?(current_user)
+            end
+            session[:sign_up_for_event] = nil
+        end
         store_original_user_logged_in(resource)
     end
 
