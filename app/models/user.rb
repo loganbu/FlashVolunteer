48b8ec159ce9b4463c1f9a303893e6cfc9000efc
Logan Buesching <agolna@gmail.com>
2012-09-10 19:47:49 -0700

    Log oauth information

--- app/models/user.rb
+++ app/models/user.rb
@@ -103,6 +103,8 @@ class User < ActiveRecord::Base
 
   def self.find_for_oauth(json_info)
     data = json_info
+    Rails.logger.info("JSON Token Information")
+    Rails.logger.info(json_info.inspect!)
     if !(user = User.where(:email => data.email).first)
       user = User.create!(:email => data.email, :password => Devise.friendly_token[0,20], :name => data.name)
       user.confirm!
