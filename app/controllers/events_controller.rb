8d5cf0fe160c875d3155ff7a033d2fa7465ece09
Logan Buesching <agolna@gmail.com>
2013-09-07 14:49:34 -0700

    Use lat/long that the app sends

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -292,7 +292,7 @@ class EventsController < ApplicationController
     per_page = params[:per_page] || 5
     proximity = params[:proximity] || ((params[:lat] || params[:long]) ? 5 : 100)
     
-    lat_long = (params[:lat] && params[:long]) ? [params[:lat].to_f, params[:long].to_f] : [47.618777, -122.33139]
+    lat_long = (params[:latitude] && params[:longitude]) ? [params[:latitude].to_f, params[:longitude].to_f] : [47.618777, -122.33139]
     @events = Event.where("1=1").near(lat_long, proximity)
     
     id_array = params[:id] && params[:id].split(',') || []
