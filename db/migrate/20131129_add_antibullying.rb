152187af664406095749bc65dc79c58f9d6d067e
Logan Buesching <agolna@gmail.com>
2013-11-20 21:18:42 -0800

    Ensure it's ordered

--- db/migrate/20131129_add_antibullying.rb
+++ db/migrate/20131129_add_antibullying.rb
@@ -1,6 +1,12 @@
 class AddAntibullying < ActiveRecord::Migration
-  def change
-    ab = Skill.new(:name => "Anti-Bullying", :offset => 19 )
+  def up
+    Skill.all.each do |s|
+        if s.offset >=2
+            s.offset += 1
+            s.save!
+        end
+    end
+    ab = Skill.new(:name => "Anti-Bullying", :offset => 2 )
     ab.save!
   end
 end
