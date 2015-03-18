730457938abbf84752f3a7c32979e8a650543143
Tyler Walker <twalker@eecs.wsu.edu>
2015-03-17 23:34:51 -0700

    added :vision to the attr_accessible

--- app/models/org.rb
+++ app/models/org.rb
@@ -2,7 +2,7 @@ class Org < User
   has_and_belongs_to_many :admins, :class_name => 'User', :join_table => 'orgs_admins', :uniq => true
   has_and_belongs_to_many :followers, :class_name => 'User', :join_table => 'orgs_followers', :uniq => true
 
-  attr_accessible :mission, :name, :email, :website
+  attr_accessible :mission, :name, :email, :website, :vision
 
   self.scope :has_admin, lambda { |u|
     includes(:admins).where('admins_users.id = ?', u.id)
